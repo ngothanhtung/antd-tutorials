645669bf7122069b339545be042428fd0020f8bb
Ngo Thanh Tung <tungnt@softech.vn>
2018-08-23 18:59:48 +0700

    Update ANT

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+
 export default class Home extends Component {
   render() {
     return (
@@ -8,6 +10,9 @@ export default class Home extends Component {
         <Button>Default</Button>
         <Button type="dashed">Dashed</Button>
         <Button type="danger">Danger</Button>
+        <Link to="/about">
+          About
+        </Link>
       </div>
     )
   }
